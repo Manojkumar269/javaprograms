2eee727dec1be58546b2a87cad9346acdaec269d
Manojkumar269 <98727879+Manojkumar269@users.noreply.github.com>
2022-03-17 17:34:08 +0530

    Add files via upload

---
+++ OnlineFlightTicketBookingSystem-BookingModule/src/main/java/com/capg/springboot/service/BookingService.java
@@ -0,0 +1,13 @@
+package com.capg.springboot.service;
+
+import com.capg.springboot.entity.Booking;
+import com.capg.springboot.dto.BookingDto;
+
+public interface BookingService
+{
+
+	Booking makePayment(BookingDto payment);
+
+	Booking cancelBooking(Booking payment);
+	
+}
