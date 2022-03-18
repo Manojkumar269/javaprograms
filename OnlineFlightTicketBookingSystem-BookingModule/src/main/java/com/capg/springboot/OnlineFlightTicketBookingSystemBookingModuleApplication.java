8ac39f7caa5d34b5d67068296b2ef64853a75567
Manojkumar269 <98727879+Manojkumar269@users.noreply.github.com>
2022-03-18 12:17:09 +0530

    Add files via upload

---
+++ OnlineFlightTicketBookingSystem-BookingModule/src/main/java/com/capg/springboot/OnlineFlightTicketBookingSystemBookingModuleApplication.java
@@ -0,0 +1,13 @@
+package com.capg.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFlightTicketBookingSystemBookingModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFlightTicketBookingSystemBookingModuleApplication.class, args);
+	}
+
+}
---
+++ OnlineFlightTicketBookingSystem-BookingModule/src/main/java/com/capg/springboot/service/BookingRepository.java
@@ -0,0 +1,18 @@
+package com.capg.springboot.service;
+
+import com.capg.springboot.dto.BookingDto;
+import com.capg.springboot.entity.Booking;
+
+public class BookingRepository {
+
+	public Booking makePayment(BookingDto payment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void cancelBooking(Object payment) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+	
